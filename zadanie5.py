d6b60b4cd6616cee9967b52fa80d1f6e03bd9313
hagrif <72786797+hagrif@users.noreply.github.com>
2021-01-18 20:12:53 +0100

    Delete zadanie5.py

--- zadanie5.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-name_list = ['Karol', 'Krzysiek', 'Barbara', 'Sebastian', 'Kacper', 'Gosia']
-first_letter_list = [element[0] for element in name_list]
-print(first_letter_list)
-
-rand_int_list = [[randint(1, 10) for element in range(4)] for element in range(5)]
-print(rand_int_list)
